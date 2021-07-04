0718b90f88183ef4244aff54678ceda27864c476
felipe <december1294@gmail.com>
2021-07-03 23:36:06 -0400

    messages

--- backend/db/seeders/20210528023836-comments.js
+++ backend/db/seeders/20210528023836-comments.js
@@ -7,85 +7,85 @@ module.exports = {
       [
         {
           user_id: 1,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 2,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 3,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 2,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 3,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 1,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 2,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 3,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 1,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 2,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 3,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 1,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 2,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
         {
           user_id: 3,
-          photo_id: 33,
+          photo_id: 3,
           body: faker.lorem.sentences(3),
 
         },
