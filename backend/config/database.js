e6cfed6486f03ecbfa4a9a1f7a39366a2f94e619
felipe <december1294@gmail.com>
2021-07-03 20:08:47 -0400

    make it better

--- backend/config/database.js
+++ backend/config/database.js
@@ -21,11 +21,5 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 };
